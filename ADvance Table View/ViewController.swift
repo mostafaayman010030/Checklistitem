53358914795b1c641dcc77cf1f91de854a2c10ed
mostafa ayman <mostafaayman010030@gmail.com>
2019-10-21 19:24:17 +0200

    Initial Commit

---
+++ ADvance Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADvance Table View
+//
+//  Created by Moustafa on 10/21/19.
+//  Copyright © 2019 Moustafa Ayman Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
